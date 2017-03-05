5b28f97cc653877a7abd0d2050e34eeaef51b591
Zuznow <chen@zuznow.com>
2017-03-05 11:32:27 +0200

    Create AppDelegate.h

---
+++ src/ios/Classes/AppDelegate.h
@@ -0,0 +1,18 @@
+/*
+ Zuznow MainViewController
+ */
+
+//
+//  AppDelegate.h
+//  HybridApp
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVAppDelegate.h>
+
+@interface AppDelegate : CDVAppDelegate {}
+
+@end
